f24140f87ce914ccf4652c8e10342ac7de227589
Nirbose <nirbose2@gmail.com>
2022-01-07 19:41:22 +0100

    Update Command classe
    Modification de la classe command

--- app/Command.php
+++ app/Command.php
@@ -6,15 +6,74 @@ class Command
 {
     public static $commands = [];
 
-    public $name;
-    public $description;
-    public $aliases = [];
-    public $ownerOnly;
-    public $boosterOnly;
-    public $permission;
-    public $invisible;
-    public $usage = 'None';
+    /**
+     * Command name
+     *
+     * @var string
+     */
+    public string $name;
 
+    /**
+     * Command description
+     *
+     * @var string
+     */
+    public string $description;
+
+    /**
+     * Command aliases
+     *
+     * @var array
+     */
+    public array $aliases = [];
+
+    /**
+     * Command permission only for owner
+     *
+     * @var string
+     */
+    public bool $ownerOnly;
+
+    /**
+     * Command permission only for booster
+     *
+     * @var string
+     */
+    public bool $boosterOnly;
+
+    /**
+     * Command permission
+     *
+     * @var string
+     */
+    public string $permission;
+
+    /**
+     * Invisible for help
+     *
+     * @var bool
+     */
+    public $invisible;
+
+    /**
+     * Usage Command exemple
+     * 
+     * @var string
+     */
+    public string $usage = 'None';
+
+    /**
+     * Command is SlashCommand
+     * 
+     * @var bool
+     */
+    public bool $slash;
+
+    /**
+     * Run commmand function
+     * 
+     * @var callable
+     */
     public $run;
 
     public function __construct(array $options)
@@ -40,6 +99,7 @@ class Command
         if(isset($options['permission'])) $this->permission = $options['permission'];
         if(isset($options['invisible'])) $this->invisible = $options['invisible'];
         if(isset($options['usage'])) $this->usage = $options['usage'];
+        if(isset($options['slash'])) $this->slash = $options['slash'];
 
         self::$commands[$options['name']] = $this;
         
