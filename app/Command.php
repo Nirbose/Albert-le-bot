ed9c230d680e438bbbd977e2fb1714b3758d83e4
Nirbose <nirbose2@gmail.com>
2022-02-24 16:20:01 +0100

    Command change structure

--- app/Command.php
+++ app/Command.php
@@ -3,198 +3,216 @@
 namespace App;
 
 use Discord\Discord;
-use Discord\Parts\Guild\Guild;
-use Discord\Parts\Interactions\Command\Command as SlashCommand;
+use Discord\Parts\Interactions\Command\Command as SlashTypeCommand;
 
+enum Context: int {
+    case MESSAGE = 1;
+    case SLASH = 2;
+}
+
+enum Type: int {
+    case ALL = 1;
+    case GUILD = 2;
+    case DM = 3;
+}
+
+/**
+ * Command class
+ * 
+ * @property string $name
+ * @property string $description
+ * @property string $usage
+ * @property string $category
+ * @property int $context
+ * @property array $aliases
+ * @property array $options
+ * @property int $type
+ * @property array $guilds
+ * @property callable $run
+ * 
+ */
 class Command
 {
 
     /**
-     * Discord client
-     *
      * @var Discord|null
      */
-    private static $discord = null;
+    private $discord = null;
 
     /**
-     * All Command
-     *
-     * @var array
-     */
-    private static array $commands = [];
-
-    /**
-     * Name Command
-     *
      * @var string
      */
-    public string $name;
+    private $name;
 
     /**
-     * Description Command
-     *
-     * @var string
-     */
-    public string $description;
-
-    /**
-     * Aliases Command
-     *
      * @var array
      */
-    public array $aliases = [];
+    private array $commands = [];
 
     /**
-     * Args Command
+     * Create command instance.
      *
-     * @var array
+     * @param Discord $discord
+     * @return void
      */
-    public array $args = [];
-
-    /**
-     * Is owner only Command
-     *
-     * @var bool
-     */
-    public bool $ownerOnly = false;
-
-    /**
-     * Is booster only Command
-     *
-     * @var bool
-     */
-    public bool $boosterOnly = false;
-
-    /**
-     * Permition Commmand
-     *
-     * @var string|null
-     */
-    public $permission = null;
-
-    /**
-     * Usage Command
-     *
-     * @var string|null
-     */
-    public string $usage = 'None';
-
-    /**
-     * Is Command slash
-     *
-     * @var boolean
-     */
-    public bool $slash = false;
-
-    /**
-     * Type SlashCommand
-     *
-     * @var int
-     */
-    public int $slashType = SlashCommand::CHAT_INPUT;
-
-    /**
-     * Options for SlashCommand
-     *
-     * @var array
-     */
-    public array $slashOptions = [];
-
-    /**
-     * Subs Command
-     * 
-     * @var array
-     */
-    public array $subs = [];
-
-    /**
-     * Command Version
-     *
-     * @var integer
-     */
-    public int $verson = 1;
-
-    /**
-     * Run Command
-     *
-     * @var callable|object
-     */
-    public $run;
-
-    public function __construct(array $options)
+    public static function create(Discord $discord): void
     {
+        $self = new self();
 
-        if (is_null(self::$discord))
-            throw new \Error("Discord n'existe pas.");
+        if ($self->discord === null) {
+            $self->discord = $discord;
+        }
+    }
 
-        if(!isset($options['name']))
-            throw new \Error("Missing name property on some Command");
+    /**
+     * Add new command
+     * 
+     * @param string $name
+     * @param callable $callback
+     */
+    public static function add(string $name, callable $callback): self
+    {
+        $self = new self();
 
-        if(!isset($options['run']))
-            throw new \Error('Missing run property on "'.$options['name'].'" Command');
+        $self->name = $name;
+        $self->commands[$name]['run'] = $callback;
 
-        $this->name = strtolower($options['name']);
-        $this->run = $options['run'];
+        return $self;
+    }
 
-        if(isset($options['description'])) {
-            $this->description = $options['description'];
+    /**
+     * Search command by name
+     * 
+     * @param string $name
+     * @return object|null
+     */
+    public static function search(string $name): object
+    {
+        $self = new self();
+
+        if (isset($self->commands[$name])) {
+            return (object)$self->commands[$name];
+        }
+
+        return null;
+    }
+
+    /**
+     * set Description of command
+     * 
+     * @param string $name
+     * @return self
+     */
+    public function setDescription(string $name): self
+    {
+        $this->commands[$this->name]['description'] = $name;
+
+        return new static();
+    }
+
+    /**
+     * set Context of command
+     * 
+     * @param Context $context
+     */
+    public function setContext(Context $context): self
+    {
+        $this->commands[$this->name]['context'] = $context;
+
+        return new static();
+    }
+
+    /**
+     * set Usage of command
+     * 
+     * @param string $usage
+     * @return self
+     */
+    public function setUsage(string $usage): self
+    {
+        $this->commands[$this->name]['usage'] = $usage;
+
+        return new static();
+    }
+
+    /**
+     * set Aliases of command
+     * 
+     * @param string $aliases
+     * @return self
+     */
+    public function setAliases(string ...$aliases): self
+    {
+        $this->commands[$this->name]['aliases'] = $aliases;
+
+        return new static();
+    }
+
+    /**
+     * set Category of command
+     * 
+     * @param string $category
+     * @return self
+     */
+    public function setCategory(string $category): self
+    {
+        $this->commands[$this->name]['category'] = $category;
+
+        return new static();
+    }
+
+    /**
+     * set Options of command
+     * 
+     * @param array $options
+     * @return self
+     */
+    public function setOptions(array $options): self
+    {
+        $this->commands[$this->name]['options'] = $options;
+
+        return new static();
+    }
+
+    /**
+     * set Type of command
+     * 
+     * @param Type|SlashTypeCommand $type
+     * @return self
+     */
+    public function setType(SlashTypeCommand $type): self
+    {
+        if ($type instanceof Type) {
+            $this->commands[$this->name]['type'] = $type->value;
         } else {
-            $this->description = ucfirst($options['name']) . " Command";
+            $this->commands[$this->name]['type'] = $type;
         }
 
-        if(isset($options['aliases'])) $this->aliases = $options['aliases'];
-        if(isset($options['ownerOnly'])) $this->ownerOnly = $options['ownerOnly'];
-        if(isset($options['boosterOnly'])) $this->boosterOnly = $options['boosterOnly'];
-        if(isset($options['permission'])) $this->permission = $options['permission'];
-        if(isset($options['usage'])) $this->usage = $options['usage'];
-        if(isset($options['args'])) $this->args = $options['args'];
-        if(isset($options['version'])) $this->version = $options['version'];
-        
-        if(isset($options['slash']) && $options['slash']) {
-            $this->slash = true;
-
-            if (isset($options['slashType'])) $this->slashType = $options['slashType'];
-            if (isset($options['slashOptions'])) $this->slashOptions = $options['slashOptions'];
-
-            $attributes = [
-                'type' => $this->slashType,
-                'name' => $this->name,
-            ];
-
-            if ($this->slashType == SlashCommand::CHAT_INPUT) {
-                $attributes['description'] = $this->description;
-                $attributes['options'] = $this->slashOptions;
-                $attributes['version'] = $this->version ?? 1;
-            }
-
-            $slashCommand = new SlashCommand(self::$discord, $attributes);
-
-            if ($options['slashGuilds'] && count($options['slashGuilds']) > 0) {
-                foreach ($options['slashGuilds'] as $guild) {
-                    self::$discord->guilds->fetch($guild)->done(function (Guild $guild) use ($slashCommand) {
-                        $guild->commands->save($slashCommand);
-                    });
-                }
-
-            } else {
-                self::$discord->application->commands->save($slashCommand);
-            }
-        } 
-
-        self::$commands[$options['name']] = $this;
-        
+        return new static();
     }
 
-    public static function getCommands() {
-        return self::$commands;
+    /**
+     * set Guilds of command.
+     * Command available only in these guilds.
+     * 
+     * @param string $guilds
+     * @return self
+     */
+    public function setGuilds(string ...$guilds): self
+    {
+        $this->commands[$this->name]['guilds'] = $guilds;
+
+        return new static();
     }
 
-    public static function init_discord(Discord $discord) {
-        if (self::$discord) {
-            return;
-        }
-
-        self::$discord = $discord;
+    /**
+     * Get
+     * 
+     * @param string $name
+     */
+    public function __get($name)
+    {
+        return $this->commands[$this->name][$name];
     }
 
 }
