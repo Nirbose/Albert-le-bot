954493d0907a88f3c0f8d54b837de99a3e332ff9
Nirbose <nirbose2@gmail.com>
2022-03-09 21:06:08 +0100

    up

--- listeners/memberJoin.php
+++ listeners/memberJoin.php
@@ -2,14 +2,17 @@
 
 use App\Database\DB;
 use App\Listener;
+use Discord\Builders\Components\ActionRow;
+use Discord\Builders\Components\Button;
 use Discord\Builders\MessageBuilder;
 use Discord\Discord;
+use Discord\Parts\Interactions\Interaction;
 use Discord\Parts\User\Member;
 use Discord\WebSockets\Event;
 
 new Listener([
     'listener' => Event::GUILD_MEMBER_ADD,
-    'run' => function (Member $member, Discord $discord) {
+    'run' => function (Member $member, Discord $discord) use ($global) {
         $sentences = DB::table('sentences')->where([
             'guildID' => $member->guild->id,
             'type' => 1,
@@ -19,6 +22,21 @@ new Listener([
 
         $channel = $discord->getChannel('781105165754433540');
 
-        $channel->sendMessage(MessageBuilder::new()->setContent($message));
+        $btn = Button::new(Button::STYLE_SECONDARY, 'welcome')
+            ->setLabel('Welcome')
+            ->setEmoji('👋');
+
+        $row = ActionRow::new();
+        $row->addComponent($btn);
+
+        $channel->sendMessage(
+            MessageBuilder::new()
+            ->setContent('<:emoji_3:829625615090712576> ' . $message)
+            ->addComponent($row)
+        );
+
+        $btn->setListener(function (Interaction $i) {
+            // Interaction was clicked
+        }, $discord);
     }
 ]);
